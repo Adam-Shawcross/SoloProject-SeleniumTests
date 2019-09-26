4e8866d53749f02c39ad3eb0276d873634ae32d8
Adam Shawcross <shawcrossadam@gmail.com>
2019-09-27 00:13:18 +0100

    CHANGED PATH ON PLAYER TESTS TO HOME PC PATH, added a delete and update selenium test

--- src/test/java/com/qa/PlayerTests.java
+++ src/test/java/com/qa/PlayerTests.java
@@ -18,7 +18,7 @@ public class PlayerTests {
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\SoloProject-SeleniumTests\\src\\test\\java\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shawc\\Desktop\\SoloProject-SeleniumTests\\src\\test\\java\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
@@ -27,6 +27,52 @@ public class PlayerTests {
     public void tearDown(){
         driver.close();
     }
+
+    @Test
+    public void deletePlayer() throws InterruptedException{
+        driver.get("http://34.89.36.254/TablePage.html");
+        Thread.sleep(3000);
+        String id = driver.findElementByXPath("/html/body/section/section[1]/table/tr[8]/td[1]").getText();
+        WebElement input = driver.findElementByXPath("//*[@id=\"playerid\"]");
+        input.sendKeys(id);
+        Thread.sleep(2000);
+        WebElement deleteButton = driver.findElementByXPath("//*[@id=\"deleteplayerform\"]/button");
+        deleteButton.click();
+        Thread.sleep(2000);
+        assertEquals(driver.findElementsByXPath("/html/body/section/section[1]/table/tr[8]/td[1]").size() == 0, true);
+
+
+    }
+
+    @Test
+    public void updatePlayer() throws InterruptedException{
+        driver.get("http://34.89.36.254/TablePage.html");
+        Thread.sleep(3000);
+        String id = driver.findElementByXPath("/html/body/section/section[1]/table/tr[8]/td[1]").getText();
+        WebElement idBox = driver.findElementByXPath("//*[@id=\"id\"]");
+        WebElement firstNameBox = driver.findElementByXPath("//*[@id=\"firstName\"]");
+        WebElement lastNameBox = driver.findElementByXPath("//*[@id=\"lastName\"]");
+        WebElement ageBox = driver.findElementByXPath("//*[@id=\"age\"]");
+        WebElement earningsBox = driver.findElementByXPath("//*[@id=\"earnings\"]");
+        WebElement submitButton = driver.findElementByXPath("//*[@id=\"bulk\"]/div/div/div[1]/form[1]/button");
+        idBox.sendKeys(id);
+        Thread.sleep(1500);
+        firstNameBox.sendKeys("Selenium Update");
+        Thread.sleep(1500);
+        lastNameBox.sendKeys("update");
+        Thread.sleep(1500);
+        ageBox.sendKeys("20");
+        Thread.sleep(1500);
+        earningsBox.sendKeys("2000");
+        Thread.sleep(1500);
+        submitButton.click();
+        Thread.sleep(2000);
+
+        assertEquals(driver.findElementByXPath("/html/body/section/section[1]/table/tr[8]/td[4]").getText(), "Selenium Update");
+    }
+
+
+
 
     @Test
     public void createPlayer() throws InterruptedException{
@@ -58,15 +104,5 @@ public class PlayerTests {
 
         assertEquals(driver.findElementByXPath("/html/body/section/section[1]/table/tr[8]/td[4]").getText(), "Selenium");
     }
-
-    @Test
-    public void deletePlayer() throws InterruptedException{
-        driver.get("http://34.89.36.254/CreatePlayer.html");
-        Thread.sleep(2000);
-        String id = driver.findElementByXPath("/html/body/section/section[1]/table/tr[8]/td[4]").getText());
-
-
-    }
-
 
 }
